4b556d371c4f714c788c897f476d2869e448d372
iphper <iguoping@qq.com>
2024-04-20 08:44:26 +0800

    v1.0.1

--- src/output/LinuxOutput.php
+++ src/output/LinuxOutput.php
@@ -9,9 +9,6 @@ class LinuxOutput extends BaseOutput
     // 获取命令行宽度
     public function sttySize(bool $force = false) : int
     {
-        if ($force == false && $this->sttySize > 0) {
-            return $this->sttySize;
-        }
         try {
             $col = trim($this->shellRun('stty size'));
             $col = explode(' ', $col)[1];
--- src/output/WinntOutput.php
+++ src/output/WinntOutput.php
@@ -9,15 +9,12 @@ class WinntOutput extends BaseOutput
     // 获取命令行宽度
     public function sttySize(bool $force = false) : int
     {
-        if ($force == false && $this->sttySize > 0) {
-            return $this->sttySize;
-        }
         try {
             $col = $this->shellRun('mode con');
             $col = preg_replace('/\s+/i', ' ', $col);
             $this->sttySize = (int) explode(" ", substr($col, strpos($col, 'Columns')))[1];
         } catch(\Exception $e) {
-
+            
         }
         return $this->sttySize;
     }
